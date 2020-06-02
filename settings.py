925d5e2622e30eab0c5e1fa30d30543390244f95
leavemyyard <zhukovpavel2001@gmail.com>
2020-06-02 12:23:36 +0300

    Now all the cases with meaning should be processed by hands

--- settings.py
+++ settings.py
@@ -8,5 +8,5 @@ overleaf_project_directory = r"C:\Users\blackbox1\Documents\GitHub\MSA-TEX"
 
 task_1_y = 1
 task_1_x = 7
-task_1_y_meaning = "Продуктивнiсть працi"
-task_1_x_meaning = "Фондовiддача"
+task_1_y_meaning = "[!Продуктивнiсть працi!]"
+task_1_x_meaning = "[!Фондовiддача!]"
